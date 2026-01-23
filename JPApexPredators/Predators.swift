c1bc95fa373517708e6c506c911b79fc20eb0c6f
Ritika <bhardwajritika11@gmail.com>
2026-01-23 23:52:03 +0530

    Created predator file for decoding the json data.

---
+++ JPApexPredators/Predators.swift
@@ -0,0 +1,32 @@
+//
+//  Predators.swift
+//  JPApexPredators
+//
+//  Created by Tarun Sharma on 23/01/26.
+//
+
+import Foundation
+
+class Predators {
+    
+    var apexPredators: [ApexPredatorModel] = []
+    
+    init() {
+        decodeApexPredators()
+    }
+    
+    
+    func decodeApexPredators() {
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json") {
+            
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let apexPredators = try decoder.decode([ApexPredatorModel].self, from: data)
+            } catch {
+                print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+}
