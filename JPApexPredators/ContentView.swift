e3a995f350489f35118fe0232a09ffd7a9b2b03c
Ritika <bhardwajritika11@gmail.com>
2026-01-25 01:30:13 +0530

    sorting functionality is done. alphabetical sorting and acesnding id sorting done.

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -12,16 +12,11 @@ struct ContentView: View {
     let predators = Predators()
     
     @State private var searchText: String = ""
+    @State private var alphabetical = false
     
     var filteredDinos: [ApexPredatorModel] {
-        if searchText.isEmpty {
-            return predators.apexPredators
-        }
-        else {
-            return predators.apexPredators.filter { predator in
-                predator.name.localizedCaseInsensitiveContains(searchText)
-            }
-        }
+        predators.sort(by: alphabetical)
+        return predators.search(for: searchText)
     }
     
     var body: some View {
@@ -63,6 +58,19 @@ struct ContentView: View {
             .searchable(text: $searchText)
             .autocorrectionDisabled()
             .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                        
+                    } label: {
+                        Image(systemName: alphabetical ? "film" : "textformat")
+                            .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+            }
         }
         .preferredColorScheme(.dark)
     }
--- JPApexPredators/Predators.swift
+++ JPApexPredators/Predators.swift
@@ -29,4 +29,27 @@ class Predators {
             }
         }
     }
+    
+    
+    public func search(for searchTerm: String) -> [ApexPredatorModel] {
+        if searchTerm.isEmpty {
+            return apexPredators
+        }
+        else {
+            return apexPredators.filter { predator in
+                predator.name.localizedCaseInsensitiveContains(searchTerm)
+            }
+        }
+    }
+    
+    public func sort(by alphabetical: Bool) {
+        return apexPredators.sort { predator1, predator2 in
+            if alphabetical {
+                predator1.name < predator2.name
+            }
+            else {
+                predator1.id < predator2.id
+            }
+        }
+    }
 }
