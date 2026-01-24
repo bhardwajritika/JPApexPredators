2d1782badfa486dfe74c5966c7b894d8e8e05ff4
Ritika <bhardwajritika11@gmail.com>
2026-01-24 18:36:22 +0530

    NavigationStack and navigation link added.

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -12,32 +12,41 @@ struct ContentView: View {
     let predators = Predators()
     
     var body: some View {
-        List(predators.apexPredators) { predator in
-            HStack {
-                // Image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
-                
-                VStack (alignment: .leading) {
-                    // name
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    
-                    // type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.backgroundColor)
-                        .clipShape(.capsule)
+        NavigationStack {
+            
+            List(predators.apexPredators) { predator in
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        // Image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        
+                        VStack (alignment: .leading) {
+                            // name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            
+                            // type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.backgroundColor)
+                                .clipShape(.capsule)
+                        }
+                    }
                 }
             }
-//            Text(predator.name)
+            .navigationTitle(Text("Apex Predators"))
         }
         .preferredColorScheme(.dark)
     }
