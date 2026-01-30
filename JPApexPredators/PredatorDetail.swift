2d62065925a87dc6699e0e13c60e752228729ff8
Ritika <bhardwajritika11@gmail.com>
2026-01-31 02:45:32 +0530

    Added gradient to the background image in the predatro detail screen

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -20,6 +20,14 @@ struct PredatorDetail: View {
                     Image(predator.type.rawValue)
                         .resizable()
                         .scaledToFit()
+                        .overlay {
+                            LinearGradient(stops: [
+                                Gradient.Stop (color: .clear, location: 0.8),
+                                Gradient.Stop(color: .black, location: 1)
+                            ] , startPoint: .top, endPoint: .bottom)
+                            
+                        }
+                        
                     
                     // dino image
                     Image(predator.image)
@@ -80,5 +88,6 @@ struct PredatorDetail: View {
 }
 
 #Preview {
-    PredatorDetail(predator: Predators().allApexPredators[2])
+    PredatorDetail(predator: Predators().allApexPredators[7])
+        .preferredColorScheme(.dark)
 }
